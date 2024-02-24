b6837ef930b8105eef678cb14d323f1dcfc4d828
Devesh Jain <deveshjain685@gmail.com>
2024-02-24 17:20:01 +0530

    added

---
+++ VVC_Leetcode_Add Two Integers/index.java
@@ -0,0 +1,11 @@
+public class index {
+    public static int sum (int num1, int num2){
+        return num1 + num2;
+    }
+    public static void main(String[] args) {
+        int num1 = -10;
+        int num2 = 4;
+        int c = sum(num1, num2);
+        System.out.println(c);
+    }
+}
